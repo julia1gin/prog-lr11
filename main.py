9cd7c83088eb297a6ef66e6cd260dc2078f2f218
Julia <keitaroromance@gmail.com>
2024-06-05 00:18:12 +0300

    Готовый pet project при помощи fastAPI

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+from contextlib import asynccontextmanager
+
+from database import create_tables, delete_tables
+from router import router as tasks_router
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print('Base cleaned')
+    await create_tables()
+    print('Base ready to work')
+    yield
+    print('Shutdown')
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
